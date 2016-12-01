e8927d0e0443ab4ffee0d2c95d8aa3fd32d9f72e
Radoslav Georgiev <rgeorgiev583@gmail.com>
2016-12-02 00:41:39 +0200

    made some members and methods private; reordered declarations; fixed style issues

--- TicTacToe/TicTacToe.h
+++ TicTacToe/TicTacToe.h
@@ -10,22 +10,25 @@ public:
     using Integer = int_fast8_t;
     static const Integer Max = 1, Min = -1, Zero = 0, Infinity = 64, Size = 9;
 
+private:
+    const TicTacToe* parent;
+    Integer move, alpha, beta;
+    TicTacToe* children[Size] {};
+
+    void Search();
+
+public:
     TicTacToe();
     ~TicTacToe();
 
-    const Integer Turn, move, Depth;
+    const Integer Turn, Depth;
 
     Integer Payoff;
-    Integer alpha, beta;
     Integer Board[Size];
 
-    const TicTacToe* parent;
-    TicTacToe *children[Size] = {};
-
-    TicTacToe *GetChild(Integer move);
+    TicTacToe* GetChild(Integer move);
 
     bool IsWin() const;
-    void Search();
     void Print() const;
 
 private:
